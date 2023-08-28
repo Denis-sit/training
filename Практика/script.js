5397f8186bfe86084d7743da42c069d7bf225018
Denis Sitnikov <specden990@mail.ru>
2023-08-28 23:09:46 +0500

    Все функции добаленны в обЬект как метож

--- Практика/script.js
+++ Практика/script.js
@@ -20,57 +20,67 @@ const personalMovieDB = {
     movies: {},
     actors: {},
     genres: [],
-    privat: false
-};
-
-function recordMyMovies(){
-    for(let i = 0; i < 2; i++){
-
-        let films = prompt('Один из просмотренных фильмов?', ''),
-            filmScore = prompt('На сколько оцените его?','');
+    privat: false,
+    start: function(){
+        numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
     
-                while(films == null || filmScore == null || films.length >= 50 || filmScore.length >= 50 || films == '' || filmScore == ''){
-                    films = prompt('Один из просмотренных фильмов?', ''),
-                    filmScore = prompt('На сколько оцените его?','');
-                    if(films != null  && filmScore !== null && films.length <= 50 && filmScore.length <= 50 && films !== '' && filmScore != '') break;
-                }
+        while(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)){
+            numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+        }
+    },
+    recordMyMovies: function (){
+        for(let i = 0; i < 2; i++){
     
-            personalMovieDB.movies[films]= filmScore;  
+            let films = prompt('Один из просмотренных фильмов?', ''),
+                filmScore = prompt('На сколько оцените его?','');
+                if(films == null || filmScore == null || films.length >= 50 || filmScore.length >= 50 || films == '' || filmScore == ''){
+                    i--;
+                };
+                personalMovieDB.movies[films]= filmScore;  
+        }
+    },
+    statusAssignment: function () {
+        if(personalMovieDB.count <= 10){
+            alert('Просмотрено довольно мало фильмов');
+        } else 
+        if(10 < personalMovieDB.count  && personalMovieDB.count <= 30){
+            alert('Вы классический зритель');
+        } else
+        if(personalMovieDB.count > 30){
+            alert('Вы киноман');
+        } else{
+            alert('Произошла ошибка');
+        }
+    },
+    showMyDB: function showMyDB(){
+        personalMovieDB.toggleVisibleMyDB(personalMovieDB.privat);
+    },
+    writeYourGenres: function (){
+        for(let i = 1; i <= 3; i++){
+            let favoriteGenre = prompt('Ваш любимый жанр под номером ' + i + '?', '');
+            if(favoriteGenre == '' || favoriteGenre == null ){
+                i--;
+            }else{
+                personalMovieDB.genres.push(favoriteGenre);
+            }
+        };
+
+        personalMovieDB.genres.forEach(function(elemet, i){
+            console.log(`Любимый жанр ${i + 1} - это ${elemet} `);
+        });
+    },
+    toggleVisibleMyDB: function (privat){
+        if(privat == false){
+            privat = true;
+        } else{
+            privat = false;
+            console.log(personalMovieDB);
+        }
     }
-}
-
-recordMyMovies();
-
-function statusAssignment() {
-    if(personalMovieDB.count <= 10){
-        alert('Просмотрено довольно мало фильмов');
-    } else 
-    if(10 < personalMovieDB.count <= 30){
-        alert('Вы классический зритель');
-    } else
-    if(personalMovieDB.count > 30){
-        alert('Вы киноман');
-    } else{
-        alert('Произошла ошибка');
-    }
-}
-
-statusAssignment();
-
-function showMyDB(privat){
-    if(!privat){
-        console.log(personalMovieDB)
-    };
 };
 
-showMyDB(personalMovieDB.privat);
-
-
-function writeYourGenres(){
-    for(let i = 1; i <= 3; i++){
-        let favoriteGenre = prompt('Ваш любимый жанр под номером ' + i + '?', '');
-        personalMovieDB.genres.push(favoriteGenre);
-    };
-};
-
-writeYourGenres();
+personalMovieDB.start();
+personalMovieDB.recordMyMovies();
+personalMovieDB.statusAssignment();
+personalMovieDB.showMyDB(personalMovieDB.privat);
+personalMovieDB.writeYourGenres();
